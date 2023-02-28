e0e5b4190461eb76a728cd4155f938321af08ef6
Agitator <1482084534@qq.com>
2023-03-01 02:31:44 +0800

    database script

---
+++ document/database.sql
@@ -0,0 +1,32 @@
+create table t_user_info
+(
+    id           int auto_increment
+        primary key,
+    name         varchar(50)                         null comment '姓名',
+    age          int                                 null comment '年龄',
+    sex          bit                                 null comment '性别 1-男 0-女',
+    phone        varchar(30)                         null comment '联系电话',
+    address      varchar(255)                        null comment '详细地址',
+    created_time timestamp default CURRENT_TIMESTAMP null,
+    deleted      int       default 0                 null comment '1-删除',
+    area_code    varchar(10)                         null comment '地址编码'
+)
+    comment '用户表';
+
+create table t_user_info_history
+(
+    id           int auto_increment
+        primary key,
+    user_id      int                                 null,
+    name         varchar(50)                         null comment '名称',
+    age          int                                 null comment '年龄',
+    phone        varchar(30)                         null comment '手机',
+    address      varchar(255)                        null comment '地址',
+    created_time timestamp default CURRENT_TIMESTAMP null comment '创建时间',
+    operate_type varchar(20)                         null comment 'update-更新 delete-删除',
+    area_code    varchar(10)                         null,
+    constraint t_user_info_history_t_user_info_fk_user_id
+        foreign key (user_id) references t_user_info (id)
+)
+    comment '用户信息更新历史';
+
